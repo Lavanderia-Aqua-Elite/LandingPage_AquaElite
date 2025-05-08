bf804a314c0ce3d24f523bd4af7e1c01a4fd3406
Vicente <vicentebravo22131@gmail.com>
2025-05-08 08:54:39 -0600

    Creando componentes y dandole diseño

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,5 +1,6 @@
 // Importa los modulos necesarios
 import React from "react";
+import classes from '../styles/Header.module.css';
 
 // Exporta por defecto y crea el componente
 export default function Header() {
@@ -7,8 +8,8 @@ export default function Header() {
         <>
 
             {/* Encabezado */}
-            <header className="header">
-                <strong className="blackText_header">AquaElite</strong>
+            <header className={classes.header}>
+                <strong className={classes.blackText_header}>AquaElite</strong>
             </header>
 
         </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 // Importa los modulos necesarios
 import React from 'react';
-import '../styles/Home.module.css';
+import classes from '../styles/Home.module.css';
 import Header from '../layouts/Header';
 
 
 // Exporta por defecto el componente y lo crea
 export default function Home() {
     return (
-        <div>
+        <div className={classes.mainHome}>
             <Header/>
         </div>
     )
