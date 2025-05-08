cf1ff373e7a53b6219d4c310df63bbc1f8b1c16e
Vicente <vicentebravo22131@gmail.com>
2025-05-07 18:09:23 -0600

    Se elimino el importamiento del index.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,9 @@
+// Importa los modulos necesarios
+import react from "@vitejs/plugin-react-swc";
+
+
+
+// Exporta por defecto el componente y lo crea
 export default function Home() {
     return (
         <div className="home_page">
