6ab21bfdea911e38e0d1d4f7c5cb298970df4817
Vicente <vicentebravo22131@gmail.com>
2025-05-07 18:05:21 -0600

    Mejore el manejo de rutas y compuse un error

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,8 +1,9 @@
 // Importa los modulos necesarios
 import React from "react";;
-import { BrowserRouter as Router , Routes, Rout } from "react-router-dom";
+import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
 
 // Importar páginas
+import Home from "../pages/Home";
 
 
 // Exporta por defecto y crea el componente para manejar las rutas
@@ -10,7 +11,7 @@ export default function AppRoutes() {
     return (
         <Router >
             <Routes >
-
+                <Route path="" element={<Home />} />
             </Routes>
         </Router>
     )
