56069dabf701565b9719e4c24b2177cbe132700b
Vicente <vicentebravo22131@gmail.com>
2025-05-08 08:30:52 -0600

    Creando el header

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,14 @@
+// Importa los modulos necesarios
+import React from "react";
+
+// Exporta por defecto y crea el componente
+export default function Header() {
+    return (
+        <>
+
+        
+
+        </>
+    )
+
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 // Importa los modulos necesarios
 import React from 'react';
-import classes from '../styles/Home.module.css'
+import '../styles/Home.module.css';
 
 
 // Exporta por defecto el componente y lo crea
 export default function Home() {
     return (
-        <div className={classes.home_page}>
+        <div>
             
         </div>
     )
