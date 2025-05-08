7020aea06f0826f8cc0cdb5068451d9c8e63e553
Vicente <vicentebravo22131@gmail.com>
2025-05-07 18:40:43 -0600

    Mejorando los enrutamiento y importando mejor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 // Importa los modulos necesarios
 import React from 'react';
-import classes from ''
+import classes from '../styles/Home.module.css'
 
 
 // Exporta por defecto el componente y lo crea
 export default function Home() {
     return (
-        <div className={home_page}>
+        <div className={classes.home_page}>
             
         </div>
     )
