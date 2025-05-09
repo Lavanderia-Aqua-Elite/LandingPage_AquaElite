76e5bdfedc6af20b33a213fb8b535127185ff2bd
Vicente <vicentebravo22131@gmail.com>
2025-05-09 15:13:57 -0600

    Creando el array de los link nav

--- src/hooks/useNavLinks.js
+++ src/hooks/useNavLinks.js
@@ -1,1 +1,35 @@
 // Importa los modulos necesarios
+import { useCallback } from "react";
+import { FaMapMarkerAlt, FaUsers, FaTools, FaEnvelope } from 'react-icons/fa';
+
+// Array para el contener la lista de enlazes
+const initialLinks = [
+    {
+        id: crypto.randomUUID(),
+        path: '#ubicacion',
+        text: 'Ubícanos',
+        icon: <FaMapMarkerAlt size={16} />,
+        visible: true
+    },
+    {
+        id: crypto.randomUUID(),
+        path: '#nosotros',
+        text: 'Sobre Nosotros',
+        icon: <FaUsers size={16} />,
+        visible: true
+    },
+    {
+        id: crypto.randomUUID(),
+        path: '#servicios',
+        text: 'Servicios',
+        icon: <FaTools size={16} />,
+        visible: true
+    },
+    {
+        id: crypto.randomUUID(),
+        path: '#contacto',
+        text: 'Contáctanos',
+        icon: <FaEnvelope size={16} />,
+        visible: true
+    }
+]
