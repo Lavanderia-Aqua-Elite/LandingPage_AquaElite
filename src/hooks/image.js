33262c29bc122fd0bb8df7fe77e5bafc20d93b69
Vicente <vicentebravo22131@gmail.com>
2025-05-09 20:49:36 -0600

    Hook para llamar imagenes

---
+++ src/hooks/image.js
@@ -0,0 +1,11 @@
+// Importa las imagenes desde su ruta especifica
+import logo from '../assets/image/AquaElite.png';
+import home from '../assets/image/home.webp';
+import washing from '../assets/image/WashingMachine.png';
+
+// Exporta por defecto las imagenes y las vuelve en js
+export default {
+    "logoPng": logo,
+    "homeWebp": home,
+    "washingPng": washing
+}
