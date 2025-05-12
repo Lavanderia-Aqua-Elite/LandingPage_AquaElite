b4579d9cd89e7ee67564f7b9cd4459769a9e6036
Vicente <vicentebravo22131@gmail.com>
2025-05-11 19:10:37 -0600

    Página de servicios

---
+++ src/pages/Service.jsx
@@ -0,0 +1,11 @@
+// Importa los modulos necesarios
+import classes from '../styles/Service.module.css';
+
+// Exporta por defecto y crea el componente
+export default function Service() {
+    return (
+        <div className={classes.contentService}>
+
+        </div>
+    )
+}
