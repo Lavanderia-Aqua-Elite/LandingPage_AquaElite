fc80a28d1f3c9f17b37183db70c4d2cb347df93b
Vicente <vicentebravo22131@gmail.com>
2025-05-13 09:11:30 -0600

    Componente para guardar textos

---
+++ src/components/Text.jsx
@@ -0,0 +1,9 @@
+// Importa los modulos necesarios
+
+export default function Text( { textdescription } ) {
+    return(
+        <div className="contentText">
+            <p className="textDescription">{textdescription}</p>
+        </div>
+    )
+}
