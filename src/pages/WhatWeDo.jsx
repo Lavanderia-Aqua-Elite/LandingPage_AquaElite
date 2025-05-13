69720afd4ffc524ff41936562c241a59ab9cd49a
Vicente <vicentebravo22131@gmail.com>
2025-05-13 15:49:39 -0600

    Mejorando la sección de servicios

--- src/pages/WhatWeDo.jsx
+++ src/pages/WhatWeDo.jsx
@@ -8,6 +8,7 @@ import laundryServiceIco from '../hooks/image';
 import MaintenanceIco from '../hooks/image';
 import shopIco from '../hooks/image';
 import MiniTitle from '../components/MiniTitle';
+import Text from '../components/Text';
 
 // Exporta por defecto y crea el componente
 export default function WhatWeDo() {
@@ -46,7 +47,55 @@ export default function WhatWeDo() {
                         />
                     </div>
                     <div className='contenService'>
-                        <MiniTitle minititle="Servicios" />
+                        <div className='mintitleService'>
+                            <MiniTitle minititle="Misión" />
+                        </div>
+                        <div className='conteinerDescription'>
+                            <p className='textDescription'>
+                                Brindar servicios de lavandería de alta calidad, ofreciendo soluciones eficientes y accesibles que garanticen la limpieza y el cuidado óptimo de las prendas de nuestros clientes, con un enfoque en la puntualidad, el respeto al medio ambiente y la satisfacción del cliente.
+                            </p>
+                        </div>
+                        <div className='mintitleService'>
+                            <MiniTitle minititle="Visión" />
+                        </div>
+                        <div className='conteinerDescription'>
+                            <p className='textDescription'>
+                                Ser la lavandería líder en la región, reconocida por la calidad de nuestros servicios, el compromiso con la sostenibilidad y la atención personalizada que ofrecemos, creando relaciones duraderas con nuestros clientes y colaboradores.
+                            </p>
+                        </div>
+                        <div className='mintitleService'>
+                            <MiniTitle minititle="Objetivo" />
+                        </div>
+                        <div className='conteinerDescription'>
+                            <p className='textDescription'>
+                                Ofrecer servicios de lavandería rápidos y eficientes, utilizando productos ecológicos y procesos innovadores que garanticen la satisfacción del cliente, al mismo tiempo que contribuimos al cuidado del medio ambiente.
+                            </p>
+                        </div>
+                        <div className='mintitleService'>
+                            <MiniTitle minititle="Valores" />
+                        </div>
+                        <div className='conteinerDescription'>
+                            <ul className='listDescription'>
+                                <li className='itemDescription'>
+                                    Calidad: Nos esforzamos por ofrecer siempre los mejores resultados en cada servicio.
+                                </li>
+                                <li className='itemDescription'>
+                                    Responsabilidad Ambiental: Utilizamos productos y procesos que minimizan el impacto ecológico.
+                                </li>
+                                <li className='itemDescription'>
+                                    Compromiso: Cumplimos puntualmente con los tiempos de entrega y los estándares de limpieza.
+                                </li>
+                                <li className='itemDescription'>
+                                    Honestidad: Actuamos con transparencia y ética en todas nuestras operaciones.
+                                </li>
+                                <li className='itemDescription'>
+                                    Servicio al Cliente: Nos enfocamos en superar las expectativas de nuestros usuarios con amabilidad y profesionalismo.
+                                </li>
+                                <li className='itemDescription'>
+                                    Innovación: Estamos en constante evolución para mejorar nuestros procesos y servicios.
+                                </li>
+                            </ul>
+                        </div>
                     </div>
             </section>
         </div>
