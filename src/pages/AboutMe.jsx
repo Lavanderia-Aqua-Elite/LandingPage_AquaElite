62b0c8b54d128eb7a3d3e6d2fb32541302a78c28
Vicente <vicentebravo22131@gmail.com>
2025-05-13 18:35:42 -0600

    Mejorando los componentes

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,9 +1,10 @@
 // Importa los modulos necesarios
+import classes from '../styles/AboutMe.module.css';
 
 // Exporta por defecto y crea el componente
 export default function AboutMe() {
     return(
-        <div className="conteinAboutMe">
+        <div className={classes.conteinAboutMe}>
 
         </div>
     )
