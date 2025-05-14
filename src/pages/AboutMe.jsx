d986997e7813e34518d73ebf4db3fa88ccf7f9c9
Vicente <vicentebravo22131@gmail.com>
2025-05-13 18:32:25 -0600

    commit

---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,10 @@
+// Importa los modulos necesarios
+
+// Exporta por defecto y crea el componente
+export default function AboutMe() {
+    return(
+        <div className="conteinAboutMe">
+
+        </div>
+    )
+}
