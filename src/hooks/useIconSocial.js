0c3b26444d6d9b154902af3dbc5e05ffbd6e95b3
Vicente Bravo <vicentebravo22131@gmail.com>
2025-06-19 17:34:18 -0600

    Modulos

--- src/hooks/useIconSocial.js
+++ src/hooks/useIconSocial.js
@@ -9,16 +9,16 @@ export default function useIconSocial(classes) {
         icon: React.createElement(FaWhatsapp, { className: `${classes.icon} ${classes.whatsapp}` }),
         url: "https://wa.me/2299316363",
         title: "WhatsApp"
-    },
-    {
-        icon: React.createElement(FaInstagram, { className: `${classes.icon} ${classes.instagram}` }),
-        url: "https://instagram.com/tu_usuario",
-        title: "Instagram"
-    },
-    {
-        icon: React.createElement(FaPhone, { className: `${classes.icon} ${classes.phone}` }),
-        url: "tel:+2299316363",
-        title: "Llamar por teléfono"
-    }
-  ];
+        },
+        {
+            icon: React.createElement(FaInstagram, { className: `${classes.icon} ${classes.instagram}` }),
+            url: "https://instagram.com/tu_usuario",
+            title: "Instagram"
+        },
+        {
+            icon: React.createElement(FaPhone, { className: `${classes.icon} ${classes.phone}` }),
+            url: "tel:+2299316363",
+            title: "Llamar por teléfono"
+        }
+    ];
 }
