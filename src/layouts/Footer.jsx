b35e07856e5dce4afaee3565a59ae04cdead25b1
Vicente Bravo <vicentebravo22131@gmail.com>
2025-06-19 16:09:53 -0600

    Resolviendo conflictos con importaciones

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,56 +1,41 @@
-//Importa componentes
+// Importa componentes
 import classes from '../styles/Footer.module.css';
 import Copyright from '../components/Copyright';
 import MiniTitle from '../components/MiniTitle';
-import { FaWhatsapp, FaInstagram, FaFacebook, FaEnvelope } from 'react-icons/fa';
+import IconSocial from '../components/IconSocial';
 
 
-//Exporta por defecto y crea el componente footer
+// Exporta por defecto y crea el componente footer
 export default function Footer() {
     return (
-
         <div className={classes.contentfoot}>
-
             <hr className={classes.line_contentfoot}/>
 
             <footer className={classes.mainfoot_contentfoot}>
-            
-            {/* Contiene el contenido del footer */}
-            
+                {/* Sección de redes sociales */}
+                <div className={classes.socialSection}>
+                    <MiniTitle 
+                        minititle="Síguenos"
+                    />
+                    <IconSocial />
+                </div>
 
-                {/* Contiene el Logo de la UNI */}
-                
-
-                {/* Contiene la descripción de la lavanderia */}
-                <MiniTitle 
-                    minititle="Sobre Nosotros"
-                />
-                
-
-                {/* Contiene la sección de icon-redes */}
-                <MiniTitle 
-                    minititle="Contactanos"
-                />
-                <IconSocial
-                    icon={FaInstagram}
-                    href="https://instagram.com/tu_usuario"
-                    altText="Instagram"
-                    color="#E1306C"
-                    hoverColor="#C13584"
-                    size={30}
-                />
-                
+                {/* Sección de contacto */}
+                <div className={classes.contactSection}>
+                    <MiniTitle 
+                        minititle="Contáctanos"
+                    />
+                    {/* Aquí podrías agregar otros elementos de contacto */}
+                </div>
 
                 <hr className={classes.line_contentfoot}/>
 
-                {/* Contiene la sección de copyraigth */}
+                {/* Sección de copyright */}
                 <Copyright 
                     company="Aqua Elite" 
                     sinceYear={2025} 
                 />
-
             </footer>
-
         </div>
     )
 }
