bb6e717628d90aea18f8cbb89b5418a6af803ce2
Vicente Bravo <vicentebravo22131@gmail.com>
2025-07-06 14:51:08 -0600

    hook para descargar el apk de la app movil

--- src/hooks/useDownloadApk.js
+++ src/hooks/useDownloadApk.js
@@ -1,7 +1,7 @@
 // Exporta y crea el hook
 export default function useDownloadApk(filename = "myapp.apk") {
     const download = () => {
-        const apkUrl = `/${filename}`;
+        const apkUrl = `/apk/${filename}`; // Ahora lo encuentra en public/apk/
         const a = document.createElement("a");
         a.href = apkUrl;
         a.download = filename;
